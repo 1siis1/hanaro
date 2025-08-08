99b6f0b30027c2ab64b6721ea694e4497aaaed38
“1siis1” <sisees01@gmail.com>
2025-08-08 22:05:09 +0900

    change architecture

--- src/main/java/com/hana7/hanaro/repository/ItemImageRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hana7.hanaro.repository;
-
-import com.hana7.hanaro.entity.ItemImage;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ItemImageRepository extends JpaRepository<ItemImage, Long> {
-}
